f4b35de7af0cfd01f57886968b717ebcb3601e50
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2022-01-29 19:58:27 +0300

    Add firebase setup

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: 'pineapples.firebaseapp.com',
+  databaseURL: 'https://pineapples-default-rtdb.firebaseio.com',
+  projectId: 'pineapples',
+  storageBucket: 'pineapples.appspot.com',
+};
+
+// initialize firebase and firestore
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firestore = getFirestore();
